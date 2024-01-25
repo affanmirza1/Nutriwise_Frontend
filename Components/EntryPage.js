6cabdeb33c71410ddb5ea131c4def9cc44ddf082
Arif Ali <arif.soomro519@gmail.com>
2024-01-25 21:09:25 +0500

    Final updated code

--- Components/EntryPage.js
+++ Components/EntryPage.js
@@ -1,131 +1,29 @@
-// import React from 'react';
-// import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-// import LottieView from 'lottie-react-native';
-
-
-// const EntryPage = () => {
-  
-//   return (
-//     <View style={styles.container}>
-//       <LottieView
-//         source={require('../assets/Animation - 1697478526157.json')} // Update with the correct path
-//         autoPlay
-//         loop
-//       />
-//       <TouchableOpacity style={styles.button1} onPress={() => console.log('Login pressed')}>
-//         <Text style={styles.buttonText}>Login</Text>
-//       </TouchableOpacity>
-//       <TouchableOpacity style={styles.button} onPress={() => console.log('Register pressed')}>
-//         <Text style={styles.buttonText}>Register</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     height: '100%',
-//     width: '100%',
-//     display: 'flex',
-//     flexDirection: 'column',
-//     backgroundColor: '#eaf7fe',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   button: {
-//     marginTop: 30,
-//     padding: 10,
-//     backgroundColor: '#3498db',
-//     borderRadius: 5,
-//   },
-//   button1: {
-//     marginTop: 570,
-//     padding: 10,
-//     backgroundColor: '#3498db',
-//     borderRadius: 5,
-//   },
-//   buttonText: {
-//     color: '#fff',
-//     fontSize: 16,
-//     fontWeight: 'bold',
-//   },
-// });
-
-// export default EntryPage;
-
-
-// import React from 'react';
-// import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-// import LottieView from 'lottie-react-native';
-// import { useNavigation } from '@react-navigation/native';
-
-// const EntryPage = () => {
-//   const navigation = useNavigation();
-
-//   const handleLoginPress = () => {
-//     navigation.navigate('Login');
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <LottieView
-//         source={require('../assets/Animation - 1697478526157.json')}
-//         autoPlay
-//         loop
-//       />
-//       <TouchableOpacity style={styles.button1} onPress={handleLoginPress}>
-//         <Text style={styles.buttonText}>Login</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     height: '100%',
-//     width: '100%',
-//     display: 'flex',
-//     flexDirection: 'column',
-//     backgroundColor: '#eaf7fe',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   button1: {
-//     marginTop: 570,
-//     padding: 10,
-//     backgroundColor: '#3498db',
-//     borderRadius: 5,
-//   },
-//   buttonText: {
-//     color: '#fff',
-//     fontSize: 16,
-//     fontWeight: 'bold',
-//   },
-// });
-
-// export default EntryPage;
-// EntryPage.js
-
-import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import LottieView from 'lottie-react-native';
+import React from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import LottieView from "lottie-react-native";
 
 const EntryPage = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <LottieView
-        source={require('../assets/Animation - 1697478526157.json')}
+        source={require("../assets/Animation - 1697478526157.json")}
         autoPlay
         loop
       />
-      <TouchableOpacity style={styles.button1} onPress={ ()=> {
-         navigation.navigate('Login')
-      }}>
+      <TouchableOpacity
+        style={styles.button1}
+        onPress={() => {
+          navigation.navigate("Login");
+        }}
+      >
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={( ) => {
-        navigation.navigate('Register')
-      }}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate("Register");
+        }}
+      >
         <Text style={styles.buttonText}>Register</Text>
       </TouchableOpacity>
     </View>
@@ -134,30 +32,30 @@ const EntryPage = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#eaf7fe',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#eaf7fe",
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
     marginTop: 30,
     padding: 10,
-    backgroundColor: '#3498db',
+    backgroundColor: "#3498db",
     borderRadius: 5,
   },
   button1: {
     marginTop: 570,
     padding: 10,
-    backgroundColor: '#3498db',
+    backgroundColor: "#3498db",
     borderRadius: 5,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
--- Components/Togglebutton.js
+++ Components/Togglebutton.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function ToggleButtons({ showMacros, showNutrients, setShowMacros, setShowNutrients }) {
+export default function ToggleButtons({
+  showMacros,
+  showNutrients,
+  setShowMacros,
+  setShowNutrients,
+}) {
   return (
     <View style={styles.toggleButtonContainer}>
       <TouchableOpacity
@@ -14,7 +19,11 @@ export default function ToggleButtons({ showMacros, showNutrients, setShowMacros
           setShowNutrients(false);
         }}
       >
-        <Text style={showMacros ? styles.activeButtonText : styles.toggleButtonText}>Macros</Text>
+        <Text
+          style={showMacros ? styles.activeButtonText : styles.toggleButtonText}
+        >
+          Macros
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={[
@@ -26,7 +35,13 @@ export default function ToggleButtons({ showMacros, showNutrients, setShowMacros
           setShowNutrients(true);
         }}
       >
-        <Text style={showNutrients ? styles.activeButtonText : styles.toggleButtonText}>Nutrients</Text>
+        <Text
+          style={
+            showNutrients ? styles.activeButtonText : styles.toggleButtonText
+          }
+        >
+          Nutrients
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -34,8 +49,8 @@ export default function ToggleButtons({ showMacros, showNutrients, setShowMacros
 
 const styles = StyleSheet.create({
   toggleButtonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginTop: 20,
   },
   toggleButton: {
@@ -43,179 +58,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 20,
     borderWidth: 1,
-    borderColor: '#197cff',
+    borderColor: "#197cff",
     marginBottom: 10,
     marginRight: 10,
   },
   activeToggleButton: {
-    backgroundColor: '#197cff',
+    backgroundColor: "#197cff",
   },
   toggleButtonText: {
-    color: 'black',
-    fontWeight: 'bold',
+    color: "black",
+    fontWeight: "bold",
   },
   activeButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
 });
-
-// import React, { useState } from 'react';
-// import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons'; // Assuming you're using Expo for icons
-
-// export default function MenuBar({ showMacros, showNutrients, setShowMacros, setShowNutrients }) {
-//   const [menuVisible, setMenuVisible] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuVisible(!menuVisible);
-//   };
-
-//   return (
-//     <View style={styles.menuBarContainer}>
-//       <TouchableOpacity
-//         style={styles.menuIcon}
-//         onPress={toggleMenu}
-//       >
-//         <Ionicons name="menu" size={24} color="black" />
-//       </TouchableOpacity>
-//       {menuVisible && (
-//         <>
-//           <TouchableOpacity
-//             style={[styles.menuItem, showMacros && styles.activeMenuItem]}
-//             onPress={() => {
-//               setShowMacros(true);
-//               setShowNutrients(false);
-//               toggleMenu(); // Close the menu
-//             }}
-//           >
-//             <Text style={styles.menuItemText}>Macros</Text>
-//           </TouchableOpacity>
-//           <TouchableOpacity
-//             style={[styles.menuItem, showNutrients && styles.activeMenuItem]}
-//             onPress={() => {
-//               setShowMacros(false);
-//               setShowNutrients(true);
-//               toggleMenu(); // Close the menu
-//             }}
-//           >
-//             <Text style={styles.menuItemText}>Nutrients</Text>
-//           </TouchableOpacity>
-//         </>
-//       )}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   menuBarContainer: {
-//     position: 'absolute',
-//     left: 0,
-//     top: 0,
-//     bottom: 0,
-//     backgroundColor: '#f0f0f0',
-//     width: 100,
-//     paddingVertical: 20,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   menuIcon: {
-//     marginBottom: 20,
-//   },
-//   menuItem: {
-//     paddingVertical: 8,
-//     paddingHorizontal: 10,
-//     borderBottomWidth: 1,
-//     borderColor: '#ccc',
-//   },
-//   activeMenuItem: {
-//     backgroundColor: '#197cff',
-//   },
-//   menuItemText: {
-//     color: 'black',
-//     fontWeight: 'bold',
-//   },
-// });
-// import React, { useState } from 'react';
-// import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons'; // Assuming you're using Expo for icons
-
-// export default function MenuBar({ showMacros, showNutrients, setShowMacros, setShowNutrients }) {
-//   const [menuVisible, setMenuVisible] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuVisible(!menuVisible);
-//   };
-
-//   return (
-//     <View style={styles.menuBarContainer}>
-//       <TouchableOpacity
-//         style={styles.menuIcon}
-//         onPress={toggleMenu}
-//       >
-//         <Ionicons name="menu" size={24} color="black" />
-//       </TouchableOpacity>
-//       <View style={styles.menuItemsContainer}>
-//         {menuVisible && (
-//           <>
-//             <TouchableOpacity
-//               style={[styles.menuItem, showMacros && styles.activeMenuItem]}
-//               onPress={() => {
-//                 setShowMacros(true);
-//                 setShowNutrients(false);
-//                 toggleMenu(); // Close the menu
-//               }}
-//             >
-//               <Text style={styles.menuItemText}>Macros</Text>
-//             </TouchableOpacity>
-//             <TouchableOpacity
-//               style={[styles.menuItem, showNutrients && styles.activeMenuItem]}
-//               onPress={() => {
-//                 setShowMacros(false);
-//                 setShowNutrients(true);
-//                 toggleMenu(); // Close the menu
-//               }}
-//             >
-//               <Text style={styles.menuItemText}>Nutrients</Text>
-//             </TouchableOpacity>
-//           </>
-//         )}
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   menuBarContainer: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     width: '100%',
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     justifyContent: 'space-between',
-//     padding: 20,
-//     backgroundColor: 'transparent', // No background until clicked
-//   },
-//   menuIcon: {},
-//   menuItemsContainer: {
-//     position: 'absolute',
-//     top: 50, // Adjust this value to position the menu items
-//     left: 0,
-//     width: '100%',
-//     backgroundColor: '#f0f0f0',
-//   },
-//   menuItem: {
-//     paddingVertical: 8,
-//     paddingHorizontal: 10,
-//     borderBottomWidth: 1,
-//     borderColor: '#ccc',
-//   },
-//   activeMenuItem: {
-//     backgroundColor: '#197cff',
-//   },
-//   menuItemText: {
-//     color: 'black',
-//     fontWeight: 'bold',
-//   },
-// });
